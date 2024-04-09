f6c27c55cd4e26d4f7a6bc8d5121c00342ec4af6
Saketh07 <107030215+GANDHAMMANI@users.noreply.github.com>
2024-04-09 22:32:57 +0530

    Update sleep_prediction_app.py

--- sleep_prediction_app.py
+++ sleep_prediction_app.py
@@ -93,6 +93,8 @@ def response_generator(user_input):
         return "Insomnia is a sleep disorder that makes it difficult for a person to fall asleep or stay asleep. It can have various causes and treatments. It's important to consult a healthcare professional if you're experiencing insomnia regularly."
     elif "apnea" in user_input.lower():
         return "Sleep apnea is a sleep disorder characterized by pauses in breathing or shallow breaths during sleep. It can lead to disrupted sleep and daytime fatigue. There are different types of sleep apnea, including obstructive sleep apnea and central sleep apnea. Treatment options may include lifestyle changes, breathing devices, or surgery. If you suspect you have sleep apnea, it's important to seek medical advice for proper diagnosis and treatment."
+    elif "develop" in user_input.lower():
+        return "The  application was build by Gandham Mani Saketh, using Python ,Machine Learning, Streamlit Web-Framework & HTML& CSS
     elif "application" in user_input.lower():
         return "The  application was build by Mani Saketh, which aims to assist users in identifying potential sleep disorders such as Insomnia and Apnea. By analyzing user input and patterns, Astra provides personalized guidance and encourages users to seek medical advice if necessary."
     elif "purpose" in user_input.lower():
